94a4aee6f38c7a7f7e2573954912a8aef92abfd7
Pyaesone Khant <pyaepsk@gmail.com>
2025-06-18 13:34:03 +0630

    feat: add create staff from

---
+++ components/admin/Staffs/CreateStaffModal.tsx
@@ -0,0 +1,125 @@
+import { Button, Flex, Modal, PasswordInput, Select, Textarea, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useDisclosure } from "@mantine/hooks";
+import { Plus } from "lucide-react";
+
+export function CreateStaffModal() {
+
+    const [opened, { toggle }] = useDisclosure(false);
+
+    const form = useForm({
+        initialValues: {
+            name: "Ei Ei",
+            email: "eiei234@gmail.com",
+            phone: "84161945",
+            address: "Singapore",
+            password: "asdfgh",
+            confirmedPassword: "asdfgh",
+            role: "STAFF"
+        },
+
+        validate: {
+            name: (value) => (value.length < 2 ? "Name must be at least 2 characters" : null),
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+            phone: (value) => (/^\d{8}$/.test(value) ? null : "Phone number must be 8 digits"),
+            address: (value) => (value.length < 5 ? "Address must be at least 5 characters" : null),
+            password: (value) => (value.length < 6 ? "Password must be at least 6 characters" : null),
+            confirmedPassword: (value, values) => (value !== values.password ? "Passwords do not match" : null),
+        }
+    });
+
+    const handleSubmit = (values: typeof form.values) => {
+        console.log(values)
+    }
+
+    return (
+        <>
+            <Button
+                leftSection={
+                    <Plus
+                        size={18}
+                    />
+                }
+            >
+                New Staff
+            </Button>
+            <Modal
+                opened={opened}
+                onClose={toggle}
+                title="Create New Staff"
+                centered
+            >
+                <form
+                    onSubmit={form.onSubmit(handleSubmit)}
+                    className="space-y-4"
+                >
+                    <TextInput
+                        label="Name"
+                        placeholder="Enter staff name"
+                        {...form.getInputProps("name")}
+                    />
+
+                    <TextInput
+                        label="Email"
+                        placeholder="Enter staff email"
+                        {...form.getInputProps("email")}
+                    />
+
+                    <TextInput
+                        label="Phone"
+                        placeholder="Enter staff phone number"
+                        {...form.getInputProps("phone")}
+                    />
+
+                    <Textarea
+                        label="Address"
+                        placeholder="Enter staff address"
+                        {...form.getInputProps("address")}
+                    />
+
+                    <PasswordInput
+                        label="Password"
+                        placeholder="Enter password"
+                        {...form.getInputProps("password")}
+                    />
+
+                    <PasswordInput
+                        label="Confirm Password"
+                        placeholder="Confirm password"
+                        {...form.getInputProps("confirmedPassword")}
+                    />
+
+                    <Select
+                        label="Role"
+                        placeholder="Select staff role"
+                        data={Object.entries(RoleEnum).map(([key, value]) => ({
+                            value: value,
+                            label: key.charAt(0) + key.slice(1).toLowerCase() // Capitalize first letter
+                        }))}
+                        {...form.getInputProps("role")}
+                    />
+
+                    <Flex
+                        gap={12}
+                        justify={"flex-end"}
+                    >
+                        <Button
+                            variant="transparent"
+                            color="gray"
+                            onClick={toggle}
+                            fullWidth
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            type="submit"
+                            fullWidth
+                        >
+                            Confirm
+                        </Button>
+                    </Flex>
+                </form>
+            </Modal>
+        </>
+    )
+}
--- constants/roles.enum.ts
+++ constants/roles.enum.ts
@@ -1,9 +1,6 @@
 
 export enum RoleEnum {
-    ADMIN = "ADMIN",
-    USER = "USER",
     DOCTOR = "DOCTOR",
-    PATIENT = "PATIENT",
     NURSE = "NURSE",
     STAFF = "STAFF",
 }
